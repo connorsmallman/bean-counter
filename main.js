072986cee4940a5726375f9321b0bc230648d96a
Connor Smallman <connor.smallman@bedegaming.com>
2017-02-22 08:31:10 +0000

    Added some layout styling Added async effect with random timeout of 1-5 seconds

--- main.js
+++ main.js
@@ -12,8 +12,10 @@ const model = {
   counters: Array.from({length: 3}, (v, i) => Object.assign({ id: i }, counter))
 };
 
+const wait = time => new Promise(resolve => setTimeout(() => resolve(), time));
+
 const counterView = (counter, actions) => html`
-  <div>
+  <div class="counter">
     <div>beans: ${counter.amount}</div>
     <div>total: ${counter.total}</div>
     <button onclick=${() => actions.increment(counter)}>Increment</button>
@@ -21,18 +23,28 @@ const counterView = (counter, actions) => html`
 `;
 
 const view = (model, actions) => html`
-  <div>${model.counters.map(c => counterView(c, actions))}</div>
+  <div class="container">${model.counters.map(c => counterView(c, actions))}</div>
 `;
 
-const reducers = {
-  increment(model, counter) {
-    counter.amount = counter.amount + 1;
-    counter.total = counter.total + 1;
+const effects = {
+  increment(model, actions, counter) {
+    actions.incrementCounter(counter);
 
-    for (const o of model.counters.filter(c => c.id !== counter.id)) {
-    	o.total = Math.max(counter.total, o.total);
+    const time = (Math.floor(Math.random() * 5) + 1) * 1000;
+
+    wait(time).then(() => actions.merge());
+  }
+}
+
+const reducers = {
+  incrementCounter(model, counter) {
+    counter.amount = counter.amount + 1;
+  },
+  merge(model) {
+    for (const c of model.counters) {
+      c.total = Math.max(c.total + 1, c.total);
     }
   }
 };
 
-app({ model, view, reducers })
+app({ model, view, reducers, effects })
